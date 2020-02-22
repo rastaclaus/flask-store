9b1b683159175a6696112956050eab0ee075a84b
Artem Shestopalov <ashestopalov@aorti.ru>
2020-02-22 03:08:28 +0300

    admin  panel

--- store/admin/admin.py
+++ store/admin/admin.py
@@ -1,20 +1,37 @@
-from flask_admin import Admin
+import os
+
+from jinja2 import Markup
+from flask import current_app as app, url_for
+
+from flask_admin import Admin, form, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.form.upload import ImageUploadField
 
 from store.models import ProductCategory, db
+admin = Admin(
+        index_view=AdminIndexView(
+            name='Администрирование',
+            url='/mod'))
 
 
-def namegen(obj, file_data):
-    return file_data.filename
+def _list_thumbnail(view, context, model, name):
+    if not model.image:
+        return ''
+
+    return Markup('<img src="{}">'.format(
+        url_for('static', filename='images/cat/'+form.thumbgen_filename(model.image))
+    ))
 
 class ProductCategoryModelView(ModelView):
+    column_formatters = {'image': _list_thumbnail}
+
     form_extra_fields = {
-        'img': ImageUploadField(
-            'Image file',
-            base_path='static/images/cat',
-            namegen=namegen)
+        'image': form.ImageUploadField(
+            'Image',
+            base_path='store/static/images/cat',
+            thumbnail_size=(100, 100, True),
+            url_relative_path='images/cat/')
     }
 
-admin = Admin()
+
 admin.add_view(ProductCategoryModelView(ProductCategory, db.session))
--- store/models/categories.py
+++ store/models/categories.py
@@ -4,4 +4,4 @@ from .db import db
 class ProductCategory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False, unique=True, index=True)
-    image = db.Column(db.LargeBinary)
+    image = db.Column(db.String(256))
